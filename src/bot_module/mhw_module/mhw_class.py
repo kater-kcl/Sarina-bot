4ea95dbca478c7930eddac3e4634b4e04698f4e2
Kater_kcl <lxd20021216@163.com>
2024-04-10 16:07:06 +0800

    修复了默认时间导致的bug

--- src/bot_module/mhw_module/mhw_class.py
+++ src/bot_module/mhw_module/mhw_class.py
@@ -1,9 +1,11 @@
 from datetime import datetime
 class AssemblyInfo:
-    def __init__(self, user_id, group_id, code, create_date=datetime.now()):
+    def __init__(self, user_id, group_id, code, create_date=None):
         self.user_id = user_id
         self.group_id = group_id
         self.assembly_code = code
+        if not create_date:
+            create_date = datetime.now()
         self.create_date = create_date
 
     def __str__(self):
